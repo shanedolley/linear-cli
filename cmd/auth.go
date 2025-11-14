fd2e4d248d0adf911d9667352a5edc1e19498070
Shane Dolley <shane@dolche.ventures>
2025-11-14 10:30:30 +1000

    refactor: update cmd/auth.go import paths

--- cmd/auth.go
+++ cmd/auth.go
@@ -4,8 +4,8 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/dorkitude/linctl/pkg/auth"
-	"github.com/dorkitude/linctl/pkg/output"
+	"github.com/shanedolley/lincli/pkg/auth"
+	"github.com/shanedolley/lincli/pkg/output"
 	"github.com/fatih/color"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
